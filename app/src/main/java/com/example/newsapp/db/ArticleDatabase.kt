0155cafa7235d9bda3180682cecc6b6560e3aac2
Srikant Achanta <srikanthachanta2@gmail.com>
2022-04-26 21:03:38 -0400

    initial commit

---
+++ app/src/main/java/com/example/newsapp/db/ArticleDatabase.kt
@@ -0,0 +1,14 @@
+package com.example.newsapp.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.newsapp.Article
+
+@Database(entities = [Article::class], version = 1)
+abstract class ArticleDatabase :RoomDatabase() {
+    abstract fun getArticleDao(): ArticleDao
+
+    companion object{
+
+    }
+}
