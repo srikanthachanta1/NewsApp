8d11ec469c4373b4c8982c56ca383240612f8417
Srikant Achanta <srikanthachanta2@gmail.com>
2022-05-05 21:24:35 -0400

    Updated to display news text

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -5,19 +5,26 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.newsapp.api.RetrofitInstance
 import com.example.newsapp.db.ArticleDatabase
+import com.example.newsapp.model.Article
 import com.example.newsapp.repository.NewsRepository
 import com.example.newsapp.ui.theme.NewsAppTheme
 import com.example.newsapp.uimodels.NewsViewModel
@@ -42,10 +49,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    //Greeting("Android")
-                    BreakingNewsScreen(){
-
+                    val news = remember {
+                        mutableStateOf("")
                     }
+                    var newsList = remember {
+                        mutableListOf<Article>()
+                    }
+                    lifecycleScope.launch {
+                        news.value = RetrofitInstance.api.getBreakingNews().body()!!.articles[1].content
+                        Log.e("NNN",""+news.value)
+                    }
+                    Column() {
+                        Greeting(news.value)
+                        //Greeting(news.value)
+                    }
+
+                    /*BreakingNewsScreen(news[1].content){
+                        Log.e("NNN","Breaking"+news.size)
+                    }*/
 
                 }
             }
@@ -55,15 +76,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "$name!")
 }
 
 @Composable
-fun BreakingNewsScreen(selectedItem: (String) -> Unit) {
+fun BreakingNewsScreen(list : String,selectedItem: (String) -> Unit) {
     LazyColumn {
-        items(100) {
+        items(20) {
             Text(
-                text = "list item $it",
+                text = "list $list",
                 style = MaterialTheme.typography.h3,
                 modifier = Modifier
                     .padding(10.dp)
